63d8d0a44df7cb7fe49f1d8f0a6b5a98a7263147
Dinith Perera <94979426+dinithshenuka@users.noreply.github.com>
2026-04-19 00:05:33 +0530

    cleanup: remove legacy files after architectural reorganization

--- internal/icdcodes/model.go
+++
@@ -1,9 +0,0 @@
-package icdcodes
-
-// ICDCode represents a clinical classification code
-type ICDCode struct {
-	ID          string `json:"id"`
-	Code        string `json:"code"`
-	Description string `json:"description"`
-	Version     string `json:"version"` // e.g., ICD-10 or ICD-11
-}
